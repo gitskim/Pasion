2ab00f64eb2e85c75f96636c96e3578b49935260
Suhyun Kim <logicdevildotcom@gmail.com>
2019-04-02 11:50:28 -0400

    added train and test

--- train_and_test.py
+++ train_and_test.py
@@ -11,11 +11,37 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import VGG16, MobileNet, MobileNetV2
 
+from keras.layers import Dense, Activation, Flatten, Dropout, BatchNormalization
+from keras import regularizers, optimizers
+import pandas as pd
+import numpy as np
+
 # TODO: fill the train dir
 TRAIN_DIR = "/home/ek2993/train"
 
 # TODO: fill the test dir
 TEST_DIR = "/home/ek2993/michael"
+
+traindf = pd.read_csv('./train.csv', header=0, encoding='unicode_escape')
+testdf = pd.read_csv("./test.csv", header=0, encoding='unicode_escape')
+
+print(traindf.head(3))
+print(testdf.head(3))
+
+traindf.columns = ['frame', 'score', 'difficulty']
+testdf.columns = ['frame', 'score', 'difficulty']
+
+print(traindf.head(3))
+print(testdf.head(3))
+
+print(traindf.tail(3))
+print(testdf.tail(3))
+
+traindf = traindf[:-1]
+testdf = testdf[:-1]
+
+print(traindf.tail(3))
+print(testdf.tail(3))
 
 # Images will be resized to(TARGET_SHAPE, TARGET_SHAPE) as they're read off disk.
 TARGET_SHAPE = 224
@@ -26,24 +52,26 @@ val_image_generator = ImageDataGenerator(rescale=1. / 255)
 
 train_data_gen = train_image_generator.flow_from_directory(directory=TRAIN_DIR,
                                                            dataframe=train_label_df,
+                                                           x_col="frame",
+                                                           y_col="score",
                                                            shuffle=True,  # Best practice: shuffle the training data
                                                            target_size=(TARGET_SHAPE, TARGET_SHAPE),
                                                            class_mode='other')
 
 val_data_gen = val_image_generator.flow_from_directory(directory=TEST_DIR,
                                                        dataframe=test_label_df,
+                                                       x_col="frame",
+                                                       y_col="score",
                                                        shuffle=True,  # Best practice: shuffle the training data
                                                        target_size=(TARGET_SHAPE, TARGET_SHAPE),
                                                        class_mode='other')
 
-conv_base = MobileNet(weights=None, include_top=False, input_shape=(224, 224, 3))
+conv_base = MobileNet(weights=None, include_top=False, input_shape=(TARGET_SHAPE, TARGET_SHAPE, 3))
 
 model = Sequential()
 model.add(conv_base)
 model.add(Flatten())
 model.add(Dense(1, activation='linear'))
-
-conv_base.trainable = False
 
 model.compile(
     optimizer=tf.train.AdamOptimizer(),
