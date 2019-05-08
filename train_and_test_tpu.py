e81ea3902d893f8ce77e33914d0b446adb002abd
Suhyun Kim <logicdevildotcom@gmail.com>
2019-05-08 18:44:11 -0400

    added tpu

---
+++ train_and_test_tpu.py
@@ -0,0 +1,33 @@
+import pose_utils as utils
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dropout, Flatten, Dense
+from tensorflow.keras import applications
+from tensorflow.keras.optimizers import SGD
+from sklearn.utils import shuffle
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.layers import LSTM
+import numpy as np
+import glob, os
+from scipy.misc import imread, imresize
+import tensorflow as tf
+
+arr_frames, arr_score, arr_difficulty = utils.get_pose_labels2()
+print(arr_frames.shape)
+
+# (298387, 104)
+timeseries = 202
+input_dim = 104
+
+model = Sequential()
+model.add(LSTM(12, dropout=0.2, input_shape=(timeseries, input_dim)))
+
+model.add(Dense(1, activation='linear'))
+model.compile(optimizer='adam', loss='mse', metrics=['mse','mae'])
+model.summary()
+
+model.fit(np.array(arr_frame_flat), np.array(arr_score), epochs=50)
+
+model.save_weights('sun-4-21-12p.h5')
+
