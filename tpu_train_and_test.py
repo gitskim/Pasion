6c4c2a81469fdda9f1b075d8e7a7bd3e724c3ea3
Suhyun Kim <logicdevildotcom@gmail.com>
2019-05-08 18:57:44 -0400

    added tpu stuff

--- tpu_train_and_test.py
+++ tpu_train_and_test.py
@@ -20,6 +20,17 @@ print(arr_frames.shape)
 timeseries = 202
 input_dim = 104
 
+arr_frames, arr_score, arr_difficulty = utils.get_pose_labels2()
+print(arr_frames.shape)
+
+# (298387, 104)
+timeseries = 202
+input_dim = 104
+
+tf.tensorflow.keras.backend.clear_session()
+
+TPU_WORKER = 'grpc://' + os.environ['COLAB_TPU_ADDR']
+
 model = Sequential()
 model.add(LSTM(12, dropout=0.2, input_shape=(timeseries, input_dim)))
 
@@ -27,7 +38,18 @@ model.add(Dense(1, activation='linear'))
 model.compile(optimizer='adam', loss='mse', metrics=['mse','mae'])
 model.summary()
 
-model.fit(arr_frames, np.array(arr_score), epochs=50)
 
-model.save_weights('sun-4-21-12p.h5')
+model.save_weights('sun-5-8-5p.h5')
 
+tpu_model = tf.contrib.tpu.keras_to_tpu_model(
+    model,
+    strategy=tf.contrib.tpu.TPUDistributionStrategy(
+        tf.contrib.cluster_resolver.TPUClusterResolver(TPU_WORKER)))
+
+tpu_model.fit_generator(
+    np.array(arr_frames), np.array(arr_score),
+    steps_per_epoch=100,
+    epochs=50,
+)
+
+tpu_model.save_weights('sun-5-8-5p.h5', overwrite=True)
